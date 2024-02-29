ba6115d7d8b34aa4f7b83325a94e79fe404536d2
root <pupa170104@gmail.com>
2024-02-29 12:44:56 +0000

    primera subida

--- www/index.php
+++ www/index.php
@@ -1,49 +1,38 @@
-<?php
-ob_start();
-session_start();
-include('inc/header.php');
-include 'Inventory.php';
-$inventory = new Inventory();
-$inventory->checkLogin();
-?>
-<script src="js/jquery.dataTables.min.js"></script>
-<script src="js/dataTables.bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/dataTables.bootstrap.min.css" />
-<script src="js/common.js"></script>
-<?php include('inc/container.php'); ?>
-<div class="container">
-	<?php include("menus.php"); ?>
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="card card-default rounded-0 shadow">
-				<div class="card-header">
-					<div class="row">
-						<div class="col-lg-10 col-md-10 col-sm-8 col-xs-6">
-							<h3 class="card-title">Lista de Inventario</h3>
-						</div>
-					</div>
-				</div>
-				<div class="card-body">
-					<div class="row">
-						<div class="col-sm-12 table-responsive">
-							<table id="inventoryDetails" class="table table-bordered table-striped">
-								<thead>
-									<tr>
-										<th>#</th>
-										<th>Producto/Código</th>
-										<th>Inventario Inicial</th>
-										<th>Inventario Recibido</th>
-										<th>Productos Vendidos</th>
-										<th>Inventario Disponible</th>
-									</tr>
-								</thead>
-							</table>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<?php require "./inc/session_start.php"; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include "./inc/head.php"; ?>
+    </head>
+    <body>
+        <?php
 
-</div>
-<?php include('inc/footer.php'); ?>
+            if(!isset($_GET['vista']) || $_GET['vista']==""){
+                $_GET['vista']="login";
+            }
+
+
+            if(is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
+
+                /*== Cerrar sesion ==*/
+                if((!isset($_SESSION['id']) || $_SESSION['id']=="") || (!isset($_SESSION['usuario']) || $_SESSION['usuario']=="")){
+                    include "./vistas/logout.php";
+                    exit();
+                }
+
+                include "./inc/navbar.php";
+
+                include "./vistas/".$_GET['vista'].".php";
+
+                include "./inc/script.php";
+
+            }else{
+                if($_GET['vista']=="login"){
+                    include "./vistas/login.php";
+                }else{
+                    include "./vistas/404.php";
+                }
+            }
+        ?>
+    </body>
+</html>
---
+++ www/vistas/home.php
@@ -0,0 +1,6 @@
+
+	<div class="container is-fluid">
+		<h1 class="title">Inicio</h1>
+		<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+	</div>
+	<div><img src="./img/lagraciosa.png" alt="lagraciosa" class="img1"></div>
